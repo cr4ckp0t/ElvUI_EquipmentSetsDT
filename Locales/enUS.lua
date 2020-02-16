d54d61ca5865c69379ee5d5e588ad68dade824bd
Adam Koch <me@adamko.ch>
2020-02-16 13:48:21 -0500

    Localization improvements.

--- Locales/enUS.lua
+++ Locales/enUS.lua
@@ -1,5 +1,5 @@
 -------------------------------------------------------------------------------
--- ElvUI_EquipmentSetsDT By Crackpotx (US, Lightbringer)
+-- ElvUI Equipment Sets Datatext By Crackpotx (US, Lightbringer)
 -------------------------------------------------------------------------------
 local debug = false
 --@debug@
@@ -9,59 +9,4 @@ local AceLocale = LibStub:GetLibrary("AceLocale-3.0")
 local L = AceLocale:NewLocale("ElvUI_EquipmentSetsDT", "enUS", true, debug)
 if not L then return end
 
-L["Alt + Click to Delete"] = true
-L["Are you sure you want to delete %s?"] = true
-L["Blue Text"] = true
-L["Changed outfit to |cff%s%s|r."] = true
-L["Choose Equipment Set"] = true
-L["Choose the outfit for keybind %d."] = true
-L["Color Text"] = true
-L["Ctrl + Click to Save"] = true
-L["Current equipment set is |cff%s%s|r."] = true
-L["Datatext Icon"] = true
-L["Debug Mode"] = true
-L["Deleted equipment set |cff%s%s|r!"] = true
-L["Display the currently equipped set's icon on the datatext, if available."] = true
-L["Display the icons for the equipment sets in the tooltip, if available."] = true
-L["ElvUI Equipment Sets Datatext"] = true
-L["Equipment Sets Datatext"] = true
-L["Equipped Items"] = true
-L["Equipped |cff%s%s|r Set"] = true
-L["Green Text"] = true
-L["Ignored Items"] = true
-L["Keybind %d"] = true
-L["Keybindings"] = true
-L["Left Click"] = true
-L["Missing Items"] = true
-L["No Equipment Sets"] = true
-L["No Equipped Set"] = true
-L["No Sets Found"] = true
-L["None"] = true
-L["Outfit 1"] = true
-L["Outfit 10"] = true
-L["Outfit 2"] = true
-L["Outfit 3"] = true
-L["Outfit 4"] = true
-L["Outfit 5"] = true
-L["Outfit 6"] = true
-L["Outfit 7"] = true
-L["Outfit 8"] = true
-L["Outfit 9"] = true
-L["Outfit not set for #%d."] = true
-L["Plugins by |cff0070deCrackpotx|r"] = true
-L["Red Text"] = true
-L["Rename %s to what?"] = true
-L["Renamed |cff%s%s|r to |cff%s%s|r!"] = true
-L["Right Click"] = true
-L["Saved |cff%s%s|r!"] = true
-L["Shift + Click to Rename"] = true
-L["Show Hint"] = true
-L["Show the hint in the tooltip."] = true
-L["Showing drop down menu."] = true
-L["Thanks for using and supporting my work!  -- |cff0070deCrackpotx|r\n\n|cffff0000If you find any bugs, or have any suggestions for any of my addons, please open a ticket at that particular addon's page on CurseForge."] = true
-L["Toggle Equipment Sets UI"] = true
-L["Toggle debug mode.  |cffff0000Useful if the addon is not functioning properly.|r"] = true
-L["Toggling Equipment Manager"] = true
-L["Tooltip Icons"] = true
-L["Total Items"] = true
-L["You can set the keybinds from within WOW's keybinding interface."] = true
+--@localization(locale="enUS", format="lua_additive_table", same-key-is-true=true, escape-non-ascii=true, handle-unlocalized="english")@
