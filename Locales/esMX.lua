f713a1fa5766c0f9bf80e5d0d29fec8abb58c440
Adam Koch <me@adamko.ch>
2023-02-18 13:25:18 -0500

    Fixed locale files.

--- Locales/esMX.lua
+++ Locales/esMX.lua
@@ -1,8 +1,8 @@
 -------------------------------------------------------------------------------
--- ElvUI Equipment Sets Datatext By Crackpot (US, Thrall)
---------------------------------------------------------------------------------
-local AceLocale = LibStub:GetLibrary("AceLocale-3.0")
-local L = AceLocale:NewLocale("ElvUI_EquipmentSetsDT", "esMX", false)
+-- ElvUI_EquipmentSetsDT By Crackpot (US, Illidan)
+-------------------------------------------------------------------------------
+local L = ElvUI[1].Libs.ACL:NewLocale("ElvUI_EquipmentSetsDT", "esMX", false)
 if not L then return end
 
 --@localization(locale="esMX", format="lua_additive_table", same-key-is-true=true, escape-non-ascii=true, handle-unlocalized="english")@
+
